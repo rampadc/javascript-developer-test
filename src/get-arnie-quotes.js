2ea8eb0eb53d33e6f5b749d1d55ef33662801e0e
Cong Nguyen <5560025+rampadc@users.noreply.github.com>
2025-06-26 13:20:11 +1000

    updated implementation explanation

--- src/get-arnie-quotes.js
+++ src/get-arnie-quotes.js
@@ -5,11 +5,11 @@ const getArnieQuotes = async (urls) => {
   // 1. Take a list of URLs
   // 2. Use the httpGet function to fetch data from each URL
   // 3. If the request is successful (status === 200), extract the quote.
-  // 4. If the request fails, the body contains something like {'FAILURE': '...'}
+  // 4. If the request fails, the body contains the reason of the failure
   // 5. All URLs should be resolved at the same time:
   //     - mock-http-interface.js injects a 200ms delay per request
   //     - get-arnie-quotes.test.js expects a total execution time of less than 400ms
-  // 6. Return a list of all objects [{'Arnie Quote': <quote> }, {'FAILURE': 'Your request has been terminated'}], in the same order as the URLs I got
+  // 6. Return a list of all objects [{'Arnie Quote': <quote> }, {'FAILURE': <reason from mock http server>}], in the same order as the URLs I got
   //     - No post processing ordering required
   //     - The order of the results for Promise.all is preserved in the order of the promises passed, regardless of completion order!
   //       - Ref: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Promise/all
